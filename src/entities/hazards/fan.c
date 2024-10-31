be30c9cb8f59ebd6a634086ab43662877064def2
julia <alara@alarabread.fun>
2024-10-31 14:06:14 -0400

    fan model

--- src/entities/hazards/fan.c
+++ src/entities/hazards/fan.c
@@ -19,17 +19,11 @@ void fanPhysicsProcess(PhysicsBody *self, double delta) {
 
 PhysicsBody *createFan(GFC_Vector3D position, GFC_Vector3D direction, float speed) {
 	gfc_vector3d_normalize(&direction);
-	GFC_Vector3D p = position;
-	p.z = 0;
-	gf3d_draw_edge_3d(
-		gfc_edge3d_from_vectors(gfc_vector3d(0, 0, 0), direction), p, gfc_vector3d(0, 0, 0), gfc_vector3d(10, 10, 10),
-		0.1, gfc_color(1, 1, 0, 1)
-	);
 	PhysicsBody *fan = physicsCreateBody();
 	GFC_Vector3D offset = {0};
 	gfc_vector3d_scale(offset, direction, FAN_DIST);
 	gfc_vector3d_add(fan->position, position, offset);
-	fan->model = gf3d_model_load("assets/models/test_cube/test_cube.model");
+	fan->model = gf3d_model_load("assets/models/fan/fan.model");
 	fan->motionType = TRIGGER;
 	GFC_Vector4D q = get_rotation_between(direction, gfc_vector3d(1, 0, 0));
 	quat_to_euler_vector(&fan->rotation, q);
